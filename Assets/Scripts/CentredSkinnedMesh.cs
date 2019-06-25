be3fd37144ddd2fa62fde976a58e376b3b06e235
WashingMachinae <juozas@unity3d.com>
2019-06-25 11:34:51 +0200

    Velocity calculation in COM struct
    Also moved vector3ClipTimeInfo out of Editor folder

--- Assets/Scripts/CentredSkinnedMesh.cs
+++ Assets/Scripts/CentredSkinnedMesh.cs
@@ -9,6 +9,19 @@ public class CentredSkinnedMesh : MonoBehaviour
     public struct centerOfMass
     {
         public Vector3 pos;
+        public Vector3 velocity;
+
+        public Vector3 KeyframeToVelocity (Vector3ClipTimeInfo clip)
+        {
+            Vector3 vel = Vector3.zero;
+
+            vel.x = (clip.currentX.value - clip.previousX.value) / (clip.currentX.time - clip.previousX.time);
+            vel.y = (clip.currentY.value - clip.previousY.value) / (clip.currentY.time - clip.previousY.time);
+            vel.z = (clip.currentZ.value - clip.previousZ.value) / (clip.currentZ.time - clip.previousZ.time);
+
+            return vel;
+        }
+
     }
 
     [Serializable]
