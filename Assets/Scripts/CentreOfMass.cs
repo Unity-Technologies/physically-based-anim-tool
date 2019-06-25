59146a9213c806ea27653c136895657ffc7bfa8a
James Bouckley <james@unity3d.com>
2019-06-25 17:51:36 +0200

    separated centre of mass to another file

---
+++ Assets/Scripts/CentreOfMass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public struct CenterOfMass
+{
+    public Vector3 pos;
+    public Vector3 velocity;
+
+    public Vector3 KeyframeToVelocity (Vector3ClipTimeInfo clip)
+    {
+        Vector3 vel = Vector3.zero;
+
+        vel.x = (clip.currentX.value - clip.previousX.value) / (clip.currentX.time - clip.previousX.time);
+        vel.y = (clip.currentY.value - clip.previousY.value) / (clip.currentY.time - clip.previousY.time);
+        vel.z = (clip.currentZ.value - clip.previousZ.value) / (clip.currentZ.time - clip.previousZ.time);
+
+        return vel;
+    }
+
+}
+
--- Assets/Scripts/CentredSkinnedMesh.cs
+++ Assets/Scripts/CentredSkinnedMesh.cs
@@ -6,25 +6,6 @@ using UnityEngine;
 public class CentredSkinnedMesh : MonoBehaviour
 {
     [Serializable]
-    public struct CenterOfMass
-    {
-        public Vector3 pos;
-        public Vector3 velocity;
-
-        public Vector3 KeyframeToVelocity (Vector3ClipTimeInfo clip)
-        {
-            Vector3 vel = Vector3.zero;
-
-            vel.x = (clip.currentX.value - clip.previousX.value) / (clip.currentX.time - clip.previousX.time);
-            vel.y = (clip.currentY.value - clip.previousY.value) / (clip.currentY.time - clip.previousY.time);
-            vel.z = (clip.currentZ.value - clip.previousZ.value) / (clip.currentZ.time - clip.previousZ.time);
-
-            return vel;
-        }
-
-    }
-
-    [Serializable]
     public struct BoneMass
     {
         public Transform bone;
