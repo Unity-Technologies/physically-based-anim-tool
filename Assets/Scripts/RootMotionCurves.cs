6a7e752742c1ab8869e7ef310d71b4ee303d3191
James Bouckley <james@unity3d.com>
2019-06-25 19:44:02 +0200

    added something potentially drawing bezier curves, still needs tangents

--- Assets/Scripts/RootMotionCurves.cs
+++ Assets/Scripts/RootMotionCurves.cs
@@ -12,8 +12,98 @@ public struct RootMotionCurves
     public AnimationCurve rootQYCurve;
     public AnimationCurve rootQZCurve;
     public AnimationCurve rootQWCurve;
+
+    List<float> m_KeyTimes;
     
     public RootMotionCurves GetTrajectoryCurves(float takeOffTime, float landTime, float gravity = -9.81f)
+    {
+        if(m_KeyTimes == null || m_KeyTimes.Count == 0)
+            SetKeyTimes();
+
+        float duration = landTime - takeOffTime;
+        Vector3 takeOffPosition = GetVector3(takeOffTime);
+        Vector3 landPosition = GetVector3(landTime);
+        Vector3 delta = landPosition - takeOffPosition;
+        
+        Keyframe[] xKeys = new Keyframe[m_KeyTimes.Count];
+        Keyframe[] yKeys = new Keyframe[m_KeyTimes.Count];
+        Keyframe[] zKeys = new Keyframe[m_KeyTimes.Count];
+
+        for (int i = 0; i < xKeys.Length; i++)
+        {
+            float time = m_KeyTimes[i];
+            xKeys[i] = new Keyframe(time, GetLateralTrajectory(time, duration, delta.x) + takeOffPosition.x);
+            yKeys[i] = new Keyframe(time, GetVerticalTrajectory(time, duration, gravity) + takeOffPosition.y);
+            zKeys[i] = new Keyframe(time, GetLateralTrajectory(time, duration, delta.z) + takeOffPosition.z);
+        }
+        
+        
+        AnimationCurve xCurve = new AnimationCurve(xKeys);
+        AnimationCurve yCurve = new AnimationCurve(yKeys);
+        AnimationCurve zCurve = new AnimationCurve(zKeys);
+
+        for (int i = 0; i < xCurve.length; i++)
+        {
+            AnimationUtility.SetKeyLeftTangentMode (xCurve, i, AnimationUtility.TangentMode.ClampedAuto);
+            AnimationUtility.SetKeyRightTangentMode (xCurve, i, AnimationUtility.TangentMode.ClampedAuto);
+        }
+
+        for (int i = 0; i < yCurve.length; i++)
+        {
+            AnimationUtility.SetKeyLeftTangentMode (yCurve, i, AnimationUtility.TangentMode.ClampedAuto);
+            AnimationUtility.SetKeyRightTangentMode (yCurve, i, AnimationUtility.TangentMode.ClampedAuto);
+        }
+
+        for (int i = 0; i < zCurve.length; i++)
+        {
+            AnimationUtility.SetKeyLeftTangentMode (zCurve, i, AnimationUtility.TangentMode.ClampedAuto);
+            AnimationUtility.SetKeyRightTangentMode (zCurve, i, AnimationUtility.TangentMode.ClampedAuto);
+        }
+
+        var curves = new RootMotionCurves();
+        curves.rootTXCurve = xCurve;
+        curves.rootTYCurve = yCurve;
+        curves.rootTZCurve = zCurve;
+        curves.m_KeyTimes = m_KeyTimes;
+        return curves;
+    }
+
+    public RootMotionCurves ConvertRootToCOM()
+    {
+        return this;    // TODO: use COM calc to implement this.
+    }
+
+    public RootMotionCurves ConvertCOMToRoot()
+    {
+        return this;    // TODO: use inverse COM calc to implement this.
+    }
+
+    public void DrawBezier()
+    {
+        if(m_KeyTimes == null || m_KeyTimes.Count == 0)
+            SetKeyTimes();
+        
+        for (int i = 0; i < m_KeyTimes.Count - 1; i++)
+        {
+            float startTime = m_KeyTimes[i];
+            float endTime = m_KeyTimes[i + 1];
+            Vector3 startPos = GetVector3(startTime);
+            Vector3 endPos = GetVector3(endTime);
+            Vector3 startTangent = Vector3.zero;    // TODO: calculate tangent for these.
+            Vector3 endTangent = Vector3.zero;
+            
+            Handles.DrawBezier(startPos, endPos, startTangent, endTangent, Color.white, null, 2f);
+        }
+    }
+
+    void SetKeyTimes()
+    {
+        float maxTime = Mathf.Max(rootTXCurve[rootTXCurve.length - 1].time, rootTYCurve[rootTYCurve.length - 1].time, rootTZCurve[rootTZCurve.length - 1].time);
+        
+        SetKeyTimes(0f, maxTime);
+    }
+
+    void SetKeyTimes(float takeOffTime, float landTime)
     {
         List<float> keyTimes = new List<float>();
 
@@ -62,63 +152,6 @@ public struct RootMotionCurves
         }
         
         keyTimes.Sort();
-
-        float duration = landTime - takeOffTime;
-        Vector3 takeOffPosition = GetVector3(takeOffTime);
-        Vector3 landPosition = GetVector3(landTime);
-        Vector3 delta = landPosition - takeOffPosition;
-        
-        Keyframe[] xKeys = new Keyframe[keyTimes.Count];
-        Keyframe[] yKeys = new Keyframe[keyTimes.Count];
-        Keyframe[] zKeys = new Keyframe[keyTimes.Count];
-
-        for (int i = 0; i < xKeys.Length; i++)
-        {
-            float time = keyTimes[i];
-            xKeys[i] = new Keyframe(time, GetLateralTrajectory(time, duration, delta.x) + takeOffPosition.x);
-            yKeys[i] = new Keyframe(time, GetVerticalTrajectory(time, duration, gravity) + takeOffPosition.y);
-            zKeys[i] = new Keyframe(time, GetLateralTrajectory(time, duration, delta.z) + takeOffPosition.z);
-        }
-        
-        
-        AnimationCurve xCurve = new AnimationCurve(xKeys);
-        AnimationCurve yCurve = new AnimationCurve(yKeys);
-        AnimationCurve zCurve = new AnimationCurve(zKeys);
-
-        for (int i = 0; i < xCurve.length; i++)
-        {
-            AnimationUtility.SetKeyLeftTangentMode (xCurve, i, AnimationUtility.TangentMode.ClampedAuto);
-            AnimationUtility.SetKeyRightTangentMode (xCurve, i, AnimationUtility.TangentMode.ClampedAuto);
-        }
-
-        for (int i = 0; i < yCurve.length; i++)
-        {
-            AnimationUtility.SetKeyLeftTangentMode (yCurve, i, AnimationUtility.TangentMode.ClampedAuto);
-            AnimationUtility.SetKeyRightTangentMode (yCurve, i, AnimationUtility.TangentMode.ClampedAuto);
-        }
-
-        for (int i = 0; i < zCurve.length; i++)
-        {
-            AnimationUtility.SetKeyLeftTangentMode (zCurve, i, AnimationUtility.TangentMode.ClampedAuto);
-            AnimationUtility.SetKeyRightTangentMode (zCurve, i, AnimationUtility.TangentMode.ClampedAuto);
-        }
-        
-        return new RootMotionCurves
-        {
-            rootTXCurve = xCurve,
-            rootTYCurve = yCurve,
-            rootTZCurve = zCurve,
-        };
-    }
-
-    public RootMotionCurves ConvertRootToCOM()
-    {
-        return this;    // TODO: use COM calc to implement this.
-    }
-
-    public RootMotionCurves ConvertCOMToRoot()
-    {
-        return this;    // TODO: use inverse COM calc to implement this.
     }
 
     Vector3 GetVector3(float time)
